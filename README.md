1ebaaea5d10467f261a9db839969fda0a3fb3f63
Amritanshu Tripathy <tripathyamritanshu7@gmail.com>
2024-03-30 16:32:06 +0530

    Script and readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # motion_extraction
-A simple bash script to extract motion from videos using ffmpeg
+A simple bash script to extract motion from videos using [ffmpeg](https://github.com/FFmpeg/FFmpeg)
+
+## Dependencies
+A bash terminal and [ffmpeg](https://github.com/FFmpeg/FFmpeg)
+
+## How it works
+First the script take the video and inverts it.
+Then the inverted video is overlaid over the original one with some delay
+The lower the delay the more minute details are visible and vice versa
+
+### Credits
+I got the original idea from [Pospy](https://www.youtube.com/watch?v=NSS6yAMZF78&t=234s)
+Do check out his YouTube channel, he does some really cool stuff
---
+++ motion_extract.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/sh
+
+echo "Enter the name of the input file: "
+read input_file
+
+echo "Enter startup delay of the tracking(with signs): "
+read startup_delay
+
+echo "Enter the transperency to be applied(between 0.0 and 1.0): "
+read alpha
+
+echo "Enter the the name of the output file(with file extension): "
+read output_file
+
+ffmpeg -i "$input_file" -vf negate inverted_file.mp4 -y
+
+ffmpeg \
+    -i "$input_file" -i inverted_file.mp4 \
+    -filter_complex " \
+        [0:v]setpts=PTS-STARTPTS, scale=480x360[top]; \
+        [1:v]setpts=PTS-STARTPTS"$startup_delay"/TB, scale=480x360, \
+             format=yuva420p,colorchannelmixer=aa="$alpha"[bottom]; \
+        [top][bottom]overlay=shortest=1" \
+    -c:v libx264 -c:a aac "$output_file" -y
+
+rm "$inverted_file"
